97c2fb4b333bca9994628f4977d06c9822e50853
Stanislaw Mnizhek <me@stanislawmnizhek.ru>
2019-01-12 03:39:12 +0300

    Add publish time and items

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import SCTrack from '../components/SCTrack'
 
+import dayjs from 'dayjs'
+
 
 const Index = (props) => (
   <div>
@@ -15,7 +17,7 @@ const Index = (props) => (
   {props.items.map((item) => (
     <div className="item">
     <video loop autoPlay>
-      <source src={item.image} type="video/mp4" />
+    <source src={item.image} type="video/mp4" />
     </video>
     <SCTrack track={item.sctrack} />
     </div>
@@ -63,24 +65,44 @@ const Index = (props) => (
   </div>
 )
 
-Index.getInitialProps = () => {
-  return {
-    items: [
-      {
-        "image": "https://media.giphy.com/media/28jTYGkICEG790UKYE/giphy.mp4",
-        "sctrack": "329505333"
-      },
-      {
-        "image": "https://media.giphy.com/media/51YdXns3IIzXbbQ7rw/giphy.mp4",
-        "sctrack": "527791200"
-      },
-      {
-        "image": "https://media.giphy.com/media/259aepyVE9wqtKIOR6/giphy.mp4",
-        "sctrack": "549103632"
-      },
-
-    ]
-  }
-}
+Index.getInitialProps = () => ({
+  items: [
+    {
+      "image": "https://media.giphy.com/media/9JrvJ16SEz3brFhzNN/giphy.mp4",
+      "sctrack": "135179524",
+      "published_at": "2019-01-15T16:00:00.000Z"
+    },
+    {
+      "image": "https://media.giphy.com/media/Ep2CkN3erKjSYmYt61/giphy.mp4",
+      "sctrack": "422768892",
+      "published_at": "2019-01-14T16:00:00.000Z"
+    },
+    {
+      "image": "https://media.giphy.com/media/1dc2MhJNVoIo0gj0Dy/giphy.mp4",
+      "sctrack": "477630402",
+      "published_at": "2019-01-13T16:00:00.000Z"
+    },
+    {
+      "image": "https://media.giphy.com/media/8OMbi7uFLuFY5gvcOT/giphy.mp4",
+      "sctrack": "297905332",
+      "published_at": "2019-01-12T16:00:00.000Z"
+    },
+    {
+      "image": "https://media.giphy.com/media/28jTYGkICEG790UKYE/giphy.mp4",
+      "sctrack": "329505333",
+      "published_at": "2019-01-11T16:00:00.000Z"
+    },
+    {
+      "image": "https://media.giphy.com/media/51YdXns3IIzXbbQ7rw/giphy.mp4",
+      "sctrack": "527791200",
+      "published_at": "2019-01-10T16:00:00.000Z"
+    },
+    {
+      "image": "https://media.giphy.com/media/259aepyVE9wqtKIOR6/giphy.mp4",
+      "sctrack": "549103632",
+      "published_at": "2019-01-10T16:00:00.000Z"
+    },
+  ].filter((item) => dayjs(item.published_at).isBefore(dayjs()))
+})
 
 export default Index
